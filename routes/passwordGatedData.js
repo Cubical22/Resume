57f0d81aaafe5c826e3e510856889db63e1fe568
Amir Hossain <tailheadeusage@gmail.com>
2023-08-21 17:28:35 +0430

    made some changes to the package.json files, and also fixed the route miss-spell on the passwordGatedData.js file, for hosting services

--- routes/passwordGatedData.js
+++ routes/passwordGatedData.js
@@ -4,19 +4,19 @@ const router = require("express").Router();
 
 router.get("/requests/:id", async (req,res)=>{
     if (req.params.id === await fs.readFile("./info.env", {encoding: "utf-8"})) {
-        res.json(await fs.readFile("./passwordGatedFiles/requests.json", {encoding: "utf-8"}));
+        res.json(await fs.readFile("./PasswordGatedFiles/requests.json", {encoding: "utf-8"}));
     }
 });
 
 router.get("/requests-remove/:id/:pass", async (req,res)=>{
     if (req.params.pass === await fs.readFile("./info.env", {encoding: "utf-8"})) {
-        const json = JSON.parse(await fs.readFile("./passwordGatedFiles/requests.json", {encoding: "utf-8"}));
+        const json = JSON.parse(await fs.readFile("./PasswordGatedFiles/requests.json", {encoding: "utf-8"}));
         const newRequestsList = json.requests.filter(request => {
             console.log("compared: ", request.index, req.params.id);
             return request.index !== JSON.parse(req.params.id);
         });
         const newJson = {requests: newRequestsList};
-        await fs.writeFile("./passwordGatedFiles/requests.json", JSON.stringify(newJson), {encoding: "utf-8"});
+        await fs.writeFile("./PasswordGatedFiles/requests.json", JSON.stringify(newJson), {encoding: "utf-8"});
 
         res.json(newJson);
 
